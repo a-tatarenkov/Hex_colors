ec65f19ef25e35c4ed62420623c1e701ad132c01
tatarenkovAlex <tatarenkov.oleksii@gmail.com>
2022-01-03 18:40:10 +0200

    gradient color

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function changeColor() {
+document.querySelector('#submit').addEventListener('click', () => {
     var hex_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    var hexcode1 = '';
+    var hexcode2 = '';
+    var random_index = 0;
 
-    var hexcode = '';
-
-    for (var i = 0; i < 6; i++) {
-        var random_index = Math.floor(Math.random() * hex_numbers.length);
-
-        hexcode += hex_numbers[random_index]
+    for (let i = 0; i < 6; i++) {
+        random_index = Math.floor(Math.random() * hex_numbers.length);
+        hexcode1 += hex_numbers[random_index];
+        random_index = Math.floor(Math.random() * hex_numbers.length);
+        hexcode2 += hex_numbers[random_index];
     }
-    document.getElementById('hex-code').innerHTML = hexcode;
-    document.getElementsByTagName('body')[0].style.background = '#' + hexcode;
-}
+    document.body.style.background = `linear-gradient(to right, #${hexcode1}, #${hexcode2})`;
+    document.querySelector("#hex-code1").textContent = hexcode1;
+    document.querySelector("#hex-code2").textContent = hexcode2;
+});
